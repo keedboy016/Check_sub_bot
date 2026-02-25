12ee0adcb2a54a56117b1e01658dedb670b1acd9
keedboy016 <ekudelin70@gmail.com>
2026-02-26 00:00:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-API_ID    = 0
-API_HASH  = "YOUR_API_HASH"
+BOT_TOKEN = os.getenv("TOKEN")
+API_ID    = os.getenv("API_ID")
+API_HASH  = os.getenv("API_HASH")
 ADMIN_ID  = 2080411409
 MAIN_CH   = "@pweper"   # обязательный канал
 DB_FILE   = "data.json"
